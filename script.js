e3f1238caa0ac1496b6698c81d32e899453689d2
Wakilat <wakilatadeola@gmail.com>
2022-06-23 10:55:25 +0100

    Intro to Javascript using Document.write function

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.write('Name: Wakilat Adeola Olanrewaju');
+document.write("<br>")
+document.write("<br>");
+document.write("Height: 168cm");
+document.write("<br>");
+document.write("<br>");
+document.write("Country: Nigeria");
